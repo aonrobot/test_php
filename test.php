b630aa41ec6dcde4beee56b306a92ab340f93b81
Auttawut Wiriyakreng <aonrobotz@gmail.com>
2016-10-05 15:40:31 +0700

    update function aon

--- test.php
+++ test.php
@@ -5,7 +5,24 @@
 	function aon(){
 		dsadsa
 		dsadsad
-		sad
+		sadfsafsaf
+		asfsa
+		fsa
+		fsafsafsa
+		fsafsafsasa
+		fbsql_affected_rowssaf
+		sadfsafsaff
+		sadfsafsaffsa
+		fbsql_affected_rowssaff
+		saf
+		sadfsafsaffsaf
+		safas
+		fbsql_affected_rowssafsa
+		fsafsafsasafas
+		fbsql_affected_rowsasf
+		saf
+		
+		return 0;
 	}
 	
 	function pop(){
